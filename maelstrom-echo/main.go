6508c69ff806ff1d5a4d955a6e55f2d554fdfacb
James Pettigrew <james@innovum.com.au>
2023-04-08 17:46:24 +1000

    feat: maelstrom-echo

---
+++ maelstrom-echo/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+
+	n.Handle("echo", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as a loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// Update the message type to return back.
+		body["type"] = "echo_ok"
+
+		// Echo the original message back with the updated message type.
+		return n.Reply(msg, body)
+	})
+
+	// Execute the node's message loop. This will run until STDIN is closed.
+	if err := n.Run(); err != nil {
+		log.Fatalf("ERROR: %s", err)
+	}
+}
