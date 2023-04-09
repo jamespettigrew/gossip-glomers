bb5d861dd670d6495f080ee76597a5a5a0156cec
James Pettigrew <james@innovum.com.au>
2023-04-09 19:56:51 +1000

    feat: maelstrom-braodcast (3c)

--- maelstrom-broadcast/go.mod
+++ maelstrom-broadcast/go.mod
@@ -2,4 +2,8 @@ module maelstrom-broadcast
 
 go 1.20
 
-require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20230321201811-151ad3cff117 // indirect
+require (
+	github.com/jepsen-io/maelstrom/demo/go v0.0.0-20230424133947-f6f4fd0d64fe // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -3,143 +3,196 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"log"
+	"os"
+	"sync"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+	log "github.com/sirupsen/logrus"
 )
 
 type (
-	BroadcastMsg struct {
-		Type  string `json:"type"`
-		Value int    `json:"message"`
-	}
-
-	TopologyMsg struct {
-		Topology map[string][]string `json:"topology"`
-	}
-
 	server struct {
-		node   *maelstrom.Node
-		nodeId string
+		node       *maelstrom.Node
+		id         string
+		messages   map[int]struct{}
+		mutex      sync.Mutex
+		neighbours map[string]*neighbour
+	}
 
-		messages             map[int]struct{}
-		messagesInserterChan chan int
-		broadcastChans       map[string]chan int
+	neighbour struct {
+		quit            chan bool
+		nodeId          string
+		unAckedMessages map[int]struct{}
+		mutex           sync.Mutex
 	}
 )
 
-func NewBroadcastMsg(value int) BroadcastMsg {
-	return BroadcastMsg{
-		Type:  "broadcast",
-		Value: value,
-	}
-}
-
 func main() {
+	f, err := os.OpenFile("/tmp/maelstrom.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	log.SetOutput(f)
+
 	n := maelstrom.NewNode()
 
 	server := server{
-		node:                 n,
-		nodeId:               n.ID(),
-		messages:             make(map[int]struct{}),
-		messagesInserterChan: make(chan int),
-		broadcastChans:       make(map[string]chan int),
+		node:       n,
+		messages:   make(map[int]struct{}),
+		neighbours: make(map[string]*neighbour),
 	}
-	go insertMessages(server.messages, server.messagesInserterChan)
 
+	n.Handle("init", server.HandleInitMsg)
 	n.Handle("broadcast", server.HandleBroadcastMsg)
 	n.Handle("read", server.HandleReadMsg)
 	n.Handle("topology", server.HandleTopologyMsg)
 
 	// Execute the node's message loop. This will run until STDIN is closed.
 	if err := n.Run(); err != nil {
-		log.Fatalf("ERROR: %s", err)
+		log.Printf("ERROR: %s", err)
+		os.Exit(1)
 	}
 }
 
-func insertMessages(messages map[int]struct{}, inserterChan <-chan int) {
-	for {
-		select {
-		case val, ok := <-inserterChan:
-			if ok {
-				messages[val] = struct{}{}
-			} else {
-				break
-			}
+func (s *server) HandleInitMsg(msg maelstrom.Message) error {
+	s.id = s.node.ID()
+
+	neighbourIds := make([]string, 0)
+	for _, neighbourId := range s.node.NodeIDs() {
+		// Don't broadcast to self
+		if neighbourId == s.node.ID() {
+			continue
 		}
+		neighbourIds = append(neighbourIds, neighbourId)
 	}
+
+	log.Printf("topology - node: %s // neighbours: %v", s.id, neighbourIds)
+	for _, neighbourId := range neighbourIds {
+		quit := make(chan bool)
+		n := &neighbour{
+			quit:            quit,
+			nodeId:          neighbourId,
+			unAckedMessages: make(map[int]struct{}),
+		}
+		s.neighbours[neighbourId] = n
+		go broadcastToNeighbour(s.node, n)
+	}
+
+	return nil
 }
 
-func (s server) HandleBroadcastMsg(msg maelstrom.Message) error {
-	var requestBody BroadcastMsg
-	if err := json.Unmarshal(msg.Body, &requestBody); err != nil {
+func (s *server) HandleBroadcastMsg(msg maelstrom.Message) error {
+	msgBody := make(map[string]any)
+	if err := json.Unmarshal(msg.Body, &msgBody); err != nil {
 		return err
 	}
 
-	// Ignore if we've already received this broadcast
-	_, ok := s.messages[requestBody.Value]
-	if ok {
-		return nil
+	var messages []int
+	if m, ok := msgBody["message"]; ok {
+		messages = append(messages, int(m.(float64)))
 	}
-	s.messagesInserterChan <- requestBody.Value
+	if m, ok := msgBody["messages"]; ok {
+		for _, m := range m.([]any) {
+			messages = append(messages, int(m.(float64)))
+		}
+	}
 
-	// Broadcast to other nodes in cluster
-	for node, c := range s.broadcastChans {
-		if node == msg.Src {
+	s.mutex.Lock()
+	log.Printf("broadcastRx - dst: %s // src: %s // msg: %v", s.id, msg.Src, messages)
+	for _, m := range messages {
+		_, ok := s.messages[m]
+		if ok {
 			continue
 		}
 
-		c <- requestBody.Value
+		s.messages[m] = struct{}{}
+	}
+	s.mutex.Unlock()
+
+	// Broadcast to other nodes in cluster
+	for neighbourId, neighbour := range s.neighbours {
+		// Don't send to node this message came from
+		if neighbourId == msg.Src {
+			continue
+		}
+
+		neighbour.mutex.Lock()
+		for _, m := range messages {
+			neighbour.unAckedMessages[m] = struct{}{}
+		}
+		neighbour.mutex.Unlock()
 	}
 
-	responseBody := make(map[string]any)
+	responseBody := make(map[string]string)
 	responseBody["type"] = "broadcast_ok"
 	return s.node.Reply(msg, responseBody)
 }
 
-func (s server) HandleReadMsg(msg maelstrom.Message) error {
+func (s *server) HandleReadMsg(msg maelstrom.Message) error {
 	responseBody := make(map[string]any)
 	responseBody["type"] = "read_ok"
 
+	s.mutex.Lock()
 	messages := []int{}
 	for m, _ := range s.messages {
 		messages = append(messages, m)
 	}
+	s.mutex.Unlock()
 
 	responseBody["messages"] = messages
 	return s.node.Reply(msg, responseBody)
 }
 
-func (s server) HandleTopologyMsg(msg maelstrom.Message) error {
-	var tm TopologyMsg
-	if err := json.Unmarshal(msg.Body, &tm); err != nil {
-		return err
-	}
-
-	for node, _ := range tm.Topology {
-		if node == s.nodeId {
-			continue
-		}
-
-		c := make(chan int, 100)
-		// Need to fill channel with existing state? If so: needs mutex?
-
-		s.broadcastChans[node] = c
-		go broadcastToNode(s.node, node, c)
-	}
-
+func (s *server) HandleTopologyMsg(msg maelstrom.Message) error {
 	responseBody := make(map[string]any)
 	responseBody["type"] = "topology_ok"
 	return s.node.Reply(msg, responseBody)
 }
 
-func broadcastToNode(sender *maelstrom.Node, dest string, messages <-chan int) {
+func broadcastToNeighbour(sender *maelstrom.Node, n *neighbour) {
+	send := func(messages []int) {
+		ctxTimeout, cancel := context.WithTimeout(context.Background(), time.Second*1)
+		defer cancel()
+
+		outgoing := make(map[string]any)
+		outgoing["type"] = "broadcast"
+		outgoing["messages"] = messages
+
+		log.Printf("broadcastTx - dst: %s // src: %s // msg: %v", sender.ID(), n.nodeId, messages)
+		_, err := sender.SyncRPC(ctxTimeout, n.nodeId, outgoing)
+		if err != nil {
+			log.Errorf("Error neighbour SyncRPC: %s", err)
+			return
+		}
+
+		n.mutex.Lock()
+		for _, m := range messages {
+			delete(n.unAckedMessages, m)
+		}
+		n.mutex.Unlock()
+	}
+
 	for {
 		select {
-		case val, ok := <-messages:
-			if ok {
-				sender.SyncRPC(context.Background(), dest, NewBroadcastMsg(val))
+		case <-n.quit:
+			return
+		default:
+			n.mutex.Lock()
+			if len(n.unAckedMessages) == 0 {
+				n.mutex.Unlock()
+				time.Sleep(time.Millisecond * 5)
+				continue
 			}
+
+			messages := make([]int, 0, len(n.unAckedMessages))
+			for m, _ := range n.unAckedMessages {
+				messages = append(messages, m)
+			}
+			n.mutex.Unlock()
+			go send(messages)
+
+			time.Sleep(time.Duration(50) * time.Millisecond)
 		}
 	}
 }
